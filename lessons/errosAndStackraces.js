601e7bcc5109936ba2e4939493709e45ac126a19
Yada Meas <yada@snapit.solutions>
2024-04-24 16:02:52 -0500

    Errors and Stacktrace examples

---
+++ lessons/errosAndStackraces.js
@@ -0,0 +1,29 @@
+// let variableA; // JS engine tolerates this.
+// console.log(variableA);
+
+// JS engine will not tolerate this
+// const variableA
+
+// You MUST define the initial value for the variable with const.
+// const variableA = "some values here";
+
+// variableB, variableC;
+
+// yourVar
+// const yourVar = "Hi!";
+// yourVar;
+// yourVar();
+
+// console.log("Hi, there!);
+// console.log"Hi, there!");
+
+// const snack = "Donuts";
+// if ((snack = "Chips")) {
+//   console.log("That will be $2.50, please!");
+// }
+
+// const funcThree = "funcThree";
+// const funcThree = () => console.log("I am the funcThree.");
+// const funcTwo = () => funcThree();
+// const funcOne = () => funcTwo();
+// funcOne();
